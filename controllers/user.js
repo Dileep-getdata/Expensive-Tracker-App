b9dea72c4237a9ce89503315a89e99f3df678cbf
Dileep Thiriveedhi <dileep2424@gmail.com>
2022-11-15 19:15:20 +0530

    Encrypted the password saving and comparing  using bcrypt.hash

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const Users=require('../models/users');
+const bcrypt=require('bcrypt');
 
 function isString(string){
     if(string==undefined || string.length===0){
@@ -17,11 +18,22 @@ exports.postSignupDetails= async(req,res)=>{
     if(isString(name) || isString(email) || isString(password)){        
        return  res.status(400).json({err:'Bad Input data'});
     }    
-    await Users.create({
-        userName:name,
-        email:email,
-        password:password})
-    res.status(200).json({message:'Successfully signed Up'})
+    const saltaround=10;
+    bcrypt.hash(password,saltaround, async(err,hash)=>{
+        if(!err){
+            if(email===Users.findAll({where:{email}})){
+                return res.status(404).json({message:'User Already Exits'})
+            }
+            await Users.create({
+                userName:name,
+                email:email,
+                password:hash})
+            res.status(200).json({message:'Successfully signed Up'})
+        }else{
+            throw new Error("Error issue at bcrypt hash");
+        }
+    });
+    
     }catch(err){
         res.status(500).json({err});
     }
@@ -38,13 +50,21 @@ exports.postLogin=(req,res)=>{
             return res.status(400).json({err:'Bad Input'});
         }
         Users.findAll({where:{email:email}})
-            .then((user)=>{            
-                if(user[0]!==undefined){                   
-                    if((user[0].password)===password){
-                        return  res.status(200).json({message:'Succesfully loged in'});
-                    }else{
-                        return res.status(401).json({message:'Wrong password'});
-                    }
+            .then((user)=>{   
+                        
+                if(user[0]!==undefined){ 
+                    bcrypt.compare(password,user[0].password,(err,result)=>{
+                        if(err){
+                            throw new Error('Something went wrong inLogin bcrypt compare');
+                        }
+                        console.log(result);
+                        if(result===true){
+                            return  res.status(200).json({message:'Succesfully loged in'});
+                        }else{
+                            return res.status(401).json({message:'Wrong password'});
+                        }
+                    })                  
+                   
                 }else{
                     return res.status(404).json({message:'Not registered Sign Up'});
                 }

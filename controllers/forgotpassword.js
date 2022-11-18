a797f17720b3fd7e80663852444a16ad91724c77
Dileep Thiriveedhi <dileep2424@gmail.com>
2022-11-18 15:00:01 +0530

    Reset password functionalties added

--- controllers/forgotpassword.js
+++ controllers/forgotpassword.js
@@ -20,10 +20,10 @@ exports.forgotPassword= async (req, res) => {
 
             const msg = {
                 to: email, // Change to your recipient
-                from: 'yj.rocks.2411@gmail.com', // Change to your verified sender
-                subject: 'Sending with SendGrid is Fun',
-                text: 'and easy to do anywhere, even with Node.js',
-                html: `<a href="http://localhost:3000/password/resetpassword/${id}">Reset password</a>`,
+                from: 'dileep2424@hotmail.com', // Change to your verified sender
+                subject: 'Resetpassword',
+                text: 'Hi There, check the below link to reset you password',
+                html: `<a href="http://localhost:4050/password/resetpassword/${id}">Reset password</a>`,
             }
 
             sgMail
@@ -46,6 +46,71 @@ exports.forgotPassword= async (req, res) => {
     } catch(err){
         console.error(err)
         return res.json({ message: err, sucess: false });
+    }
+
+}
+
+
+exports.resetpassword = (req, res) => {
+    const id =  req.params.id;
+    Forgotpass.findOne({ where : { id }}).then(forgotpasswordrequest => {
+        if(forgotpasswordrequest){
+            forgotpasswordrequest.update({ active: false});
+            res.status(200).send(`<html>
+                                    <script>
+                                        function formsubmitted(e){
+                                            e.preventDefault();
+                                            console.log('called')
+                                        }
+                                    </script>
+                                    <form action="/password/updatepassword/${id}" method="get">
+                                        <label for="newpassword">Enter New password</label>
+                                        <input name="newpassword" type="password" required></input>
+                                        <button>reset password</button>
+                                    </form>
+                                </html>`
+                                )
+            res.end()
+
+        }
+    })
+}
+
+exports.updatepassword = (req, res) => {
+
+    try {
+        const { newpassword } = req.query;
+        const { resetpasswordid } = req.params;
+        Forgotpass.findOne({ where : { id: resetpasswordid }}).then(resetpasswordrequest => {
+            User.findOne({where: { id : resetpasswordrequest.userId}}).then(user => {
+                // console.log('userDetails', user)
+                if(user) {
+                    //encrypt the password
+
+                    const saltRounds = 10;
+                    bcrypt.genSalt(saltRounds, function(err, salt) {
+                        if(err){
+                            console.log(err);
+                            throw new Error(err);
+                        }
+                        bcrypt.hash(newpassword, salt, function(err, hash) {
+                            // Store hash in your password DB.
+                            if(err){
+                                console.log(err);
+                                throw new Error(err);
+                            }
+                            user.update({ password: hash }).then(() => {
+                                res.status(201).json({message: 'Successfuly update the new password'})
+                            })
+                        });
+                    });
+            } else{
+                return res.status(404).json({ error: 'No user Exists', success: false})
+            }
+            })
+        })
+    } catch(error){
+        return res.status(403).json({ error, success: false } )
     }
 
 }

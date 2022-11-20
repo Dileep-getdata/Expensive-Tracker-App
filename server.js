ea92310f2d737821c40943333410007b6cf73054
Dileep Thiriveedhi <dileep2424@gmail.com>
2022-11-20 13:41:39 +0530

    main server

--- server.js
+++ server.js
@@ -45,6 +45,10 @@ app.use('/expenses',expensesRouter);
 app.use('/purchase',paymentRouter);
 app.use('/password',forgotPassRouter);
 
+app.use((req,res)=>{
+    res.sendFile(path.join(__dirname,`Front end/Signup/Signup.html`));
+})
+
 Users.hasMany(Expenses);
 Expenses.belongsTo(Users);
 
